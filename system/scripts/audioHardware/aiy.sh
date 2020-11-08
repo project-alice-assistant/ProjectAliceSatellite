9feb85bf9f7291fcb4fdd7a4581b26ab36e05f2f
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-11-08 13:08:04 +0100

    :bug: Updating audio hardware install scripts

--- system/scripts/audioHardware/aiy.sh
+++ system/scripts/audioHardware/aiy.sh
@@ -5,22 +5,22 @@
 # https://github.com/google/aiyprojects-raspbian/blob/voicekit/scripts/configure-driver.sh
 
 if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root (use sudo)" 1>&2
-  exit 1
+   echo "This script must be run as root (use sudo)" 1>&2
+   exit 1
 fi
 
 set -e
 
 sed -i \
--e "s/^dtparam=audio=on/#\0/" \
--e "s/^#\(dtparam=i2s=on\)/\1/" \
-/boot/config.txt
+  -e "s/^dtparam=audio=on/#\0/" \
+  -e "s/^#\(dtparam=i2s=on\)/\1/" \
+  /boot/config.txt
 grep -q "dtoverlay=i2s-mmap" /boot/config.txt || \
-echo "dtoverlay=i2s-mmap" >>/boot/config.txt
+  echo "dtoverlay=i2s-mmap" >> /boot/config.txt
 grep -q "dtoverlay=googlevoicehat-soundcard" /boot/config.txt || \
-echo "dtoverlay=googlevoicehat-soundcard" >>/boot/config.txt
+  echo "dtoverlay=googlevoicehat-soundcard" >> /boot/config.txt
 grep -q "dtparam=i2s=on" /boot/config.txt || \
-echo "dtparam=i2s=on" >>/boot/config.txt
+  echo "dtparam=i2s=on" >> /boot/config.txt
 
 set -o errexit
 
@@ -30,7 +30,7 @@ asoundrc=/home/pi/.asoundrc
 global_asoundrc=/etc/asound.conf
 
 for rcfile in "$asoundrc" "$global_asoundrc"; do
-  if [[ -f "$rcfile" ]]; then
+  if [[ -f "$rcfile" ]] ; then
     echo "Renaming $rcfile to $rcfile.bak..."
     sudo mv "$rcfile" "$rcfile.bak"
   fi
--- system/scripts/audioHardware/respeakers.sh
+++ system/scripts/audioHardware/respeakers.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-cd /home/$(logname) || exit
+cd ~ || exit
 
-if [[ -d "/home/$(logname)/seeed-voicecard" ]]; then
-  rm -rf /home/$(logname)/seeed-voicecard
+if [[ -d "seeed-voicecard" ]]; then
+  rm -rf seeed-voicecard
 fi
 
-git clone https://github.com/respeaker/seeed-voicecard.git /home/$(logname)/seeed-voicecard
-
-sed -i -e 's/  install_kernel/#  install_kernel/' /home/$(logname)/seeed-voicecard/install.sh
-sed -i -e 's/  check_kernel_headers/#  check_kernel_headers/' /home/$(logname)/seeed-voicecard/install.sh
-
-
-chmod +x /home/$(logname)/seeed-voicecard/install.sh
-./home/$(logname)/seeed-voicecard/install.sh
+# git clone https://github.com/respeaker/seeed-voicecard.git
+# use alt repo that works with latest kernel without downgrading
+git clone https://github.com/HinTak/seeed-voicecard.git
+cd seeed-voicecard || exit
+git checkout v5.5
+git pull
+chmod +x ./install.sh
+./install.sh --compat-kernel
 
 sleep 1
-
-systemctl enable seeed-voicecard
