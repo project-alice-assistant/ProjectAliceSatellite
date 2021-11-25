28d9ea5c7a8ead6c91a85bc7048ddb0151565658
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-11-25 14:20:19 +0100

    Update audio scripts

--- system/scripts/audioHardware/respeakers.sh
+++ system/scripts/audioHardware/respeakers.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+#
+# Copyright (c) 2021
+#
+# This file, respeakers.sh, is part of Project Alice.
+#
+# Project Alice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+#
+# Last modified: 2021.04.13 at 12:56:49 CEST
+#
+
 cd ~ || exit
 
 if [[ -d "seeed-voicecard" ]]; then
@@ -10,9 +31,9 @@ fi
 # use alt repo that works with latest kernel without downgrading
 git clone https://github.com/HinTak/seeed-voicecard.git
 cd seeed-voicecard || exit
-git checkout v5.5
+git checkout v5.9
 git pull
 chmod +x ./install.sh
-./install.sh --compat-kernel
+./install.sh
 
 sleep 1
